a1574432844ecbbe7b8448abf9da479a816f1eb7
Baran Kazan <baran.kazan@hotmail.com>
2023-02-08 18:32:08 +0100

    Made it more "advance"

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 import interactions
 
-bot = interactions.Client(token="MTA2NTY1ODMxNzQ4NzIxMDQ5Ng.G73OY0.8iahaQ0iU1Z-UeT-3i5hAFno_kZFcGoZ3yZtoE")
+bot = interactions.Client(
+    token="MTA2NTY1ODMxNzQ4NzIxMDQ5Ng.GYi6X4.i4UDls7mgQwPgCbYXyqtsofFClrMr3-Or9u9zw",
+    default_scope=622450248362754050
+)
 
 @bot.command(
     name="my_first_command",
     description="This is the first command I made!",
+    options = [
+        interactions.Option(
+            name="text",
+            description="What you want to say",
+            type=interactions.OptionType.STRING,
+            required=True,
+        ),
+    ],
 )
-async def my_first_command(ctx: interactions.CommandContext):
-    await ctx.send("Hi there!")
+async def my_first_command(ctx: interactions.CommandContext , text:str):
+    #await test(ctx)
+    await ctx.send(f"The user {ctx.user.username} says {text}")
+
+"""
+@bot.command(
+    type=interactions.ApplicationCommandType.USER,
+    name="User Command",
+)
+async def test(ctx):
+    await ctx.send(f"You have applied a command onto user {ctx.target.user.username}!")
+"""
 
 bot.start()
