35413238af1d055de1e6b7f01c661bc5dd58fa36
Baran Kazan <Baran.kazan@hotmail.com>
2023-02-23 19:52:40 +0100

    Typo correction

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import os
 import interactions
 
 guild_id = os.getenv("GUILD_ID")
-client_secret = os.getenv("CLIENT_SECRET")
+token = os.getenv("TOKEN")
 
 bot = interactions.Client(
-    token=client_secret,
+    token=token,
     default_scope=guild_id
 )
 
@@ -15,6 +15,7 @@ bot = interactions.Client(
     description="It will return 3rd party app connected to the users",
 )
 async def my_first_command(ctx: interactions.CommandContext):
+    user_json = get
     await ctx.send(f"The user {ctx.user.username}")
 
 bot.start()
