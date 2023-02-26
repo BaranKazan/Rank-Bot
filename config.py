88a19bba5b1a244d5f018f995a9e50ec73bc608b
Baran Kazan <Baran.kazan@hotmail.com>
2023-02-26 19:32:00 +0100

    Added CLIENT_ID

--- config.py
+++ config.py
@@ -7,3 +7,4 @@ GUILD_ID = 622450248362754050
 REDIRECT_URI = "http://127.0.0.1:5000/oauth/callback"
 OAUTH_URL= f"https://discord.com/api/oauth2/authorize?client_id=1065658317487210496&redirect_uri={parse.quote(REDIRECT_URI)}&response_type=code&scope=identify%20guilds"
 SECRET_KEY = os.urandom(24)
+CLIENT_ID = 1065658317487210496
